ef6fb7332d2871ae2d32bb38ebaebb2bbbff3a71
Thiago Cruz <thiagocruz.eu@gmail.com>
2024-10-11 20:14:00 -0300

    update theme colors

--- src/admin/app.tsx
+++ src/admin/app.tsx
@@ -19,6 +19,18 @@ export default {
         "app.components.LeftMenu.navbrand.title": "Won Games Dashboard"
       },
     },
+    theme: {
+      light: {},
+      dark: {
+        colors: {
+          primary100: "#030415",
+          primary600: "#f231a5",
+          primary700: "#f231a5",
+          neutral0: "#0d102f",
+          neutral100: "#030415",
+        }
+      },
+    },
     locales: [],
   },
   bootstrap() {},
