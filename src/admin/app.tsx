f2a6db47d418d6e60a1618fac35e57f3f401f050
Thiago Cruz <thiagocruz.eu@gmail.com>
2024-10-11 19:32:54 -0300

    config brand icon and logo

---
+++ src/admin/app.tsx
@@ -0,0 +1,18 @@
+import Icon from './extensions/icon.png';
+import Logo from './extensions/logo.svg';
+
+export default {
+  config: {
+    auth: {
+      logo: Logo,
+    },
+    head: {
+      favicon: Icon,
+    },
+    menu: {
+      logo: Icon,
+    },
+    locales: [],
+  },
+  bootstrap() {},
+};
